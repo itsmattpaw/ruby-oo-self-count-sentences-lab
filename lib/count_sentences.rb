023ae29578abd589e922d5be088d2b6416d9ac20
itsmattpaw <itsmattpaw@gmail.com>
2022-02-22 06:44:53 -0500

    commit tree 617043bcb7c0061806920dbfb0bfe6e40b165c4f

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".") then true else false end
   end
 
   def question?
-
+    if self.end_with?("?") then true else false end
   end
 
   def exclamation?
-
+    if self.end_with?("!") then true else false end
   end
 
   def count_sentences
-
+    self.split(/(?=\.\s|\!\s|\?\s)/).count
+    #self.split(".","!","?").count
   end
 end
